85c7bab3684088a252de0fe701fcb5dc50e367ae
Eshanhasitha <eshanhasitha5@gmail.com>
2025-03-09 11:03:54 +0530

    I changed the code to get the number of rows from the user.

--- pyramid.c
+++ pyramid.c
@@ -1,13 +1,15 @@
 #include<stdio.h>
 int main()
 {
-    
+    int rows;
+    printf("Enter the number of rows: ");
+    scanf("%d",&rows);
     //outer loop for rows
-    for(int i=1;i<=5;i++)
+    for(int i=1;i<=rows;i++)
     {
         
         //first inner loop for spaces
-        for (int j=5;j>=i;j--)
+        for (int j=1;j<=rows-i;j++)
         {
             printf(" ");
         }  
@@ -15,16 +17,10 @@ int main()
         //second inner loop for printing stars
         for(int k=1;k<=i;k++){
             
-            printf("*");
+            printf(" *");
                   
         }
-        //third inner loop for printing stars
-        for (int l=2;l<=i;l++)
-        {
-            printf("*");
-        } 
-        printf("\n");
-          
+        printf("\n");  
     }
     
    
