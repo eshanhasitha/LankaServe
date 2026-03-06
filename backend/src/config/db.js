bd47f08229527a9cb29eb8efc8f924d9ba52204d
Eshanhasitha <eshanhasitha5@gmail.com>
2026-03-06 09:01:29 +0530

    Setup backed folder structure, connect mongoDB and firebase initialized

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,8 +1,8 @@
 import mongoose from "mongoose";
 
-const connectDB = async () => {
+export const connectDB = async (mongoUri = process.env.MONGO_URI) => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
+    const conn = await mongoose.connect(mongoUri);
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`Error: ${error.message}`);
