7e5ad3b2e19d2749298230451996f910c19556f5
Chathmi <your-email@example.com>
2026-03-28 15:33:46 +0530

    Add QR scanner page, update api_service and add camera permission

--- mobile/lib/api_service.dart
+++ mobile/lib/api_service.dart
@@ -1,35 +1,11 @@
 import 'package:dio/dio.dart';
 
 class ApiService {
-  static final Dio dio = Dio(BaseOptions(
-    baseUrl: 'http://10.0.2.2:5000/api',
-    headers: {'Content-Type': 'application/json'},
-  ));
+  static final dio = Dio(BaseOptions(baseUrl: 'http://10.0.2.2:5000/api'));
 
-  static Future<Map<String, dynamic>> login(String token) async {
-    final res = await dio.post('/auth/login', data: {'firebaseIdToken': token});
-    return res.data['data'];
-  }
-
-  static Future<List<dynamic>> getJobs(String accessToken) async {
-    final res = await dio.get(
-      '/jobs',
-      options: Options(headers: {'Authorization': 'Bearer $accessToken'}),
-    );
-    return res.data['data'] ?? [];
-  }
-
-  static Future<void> acceptJob(String accessToken, String jobId) async {
-    await dio.put(
-      '/jobs/$jobId/accept',
-      options: Options(headers: {'Authorization': 'Bearer $accessToken'}),
-    );
-  }
-
-  static Future<void> rejectJob(String accessToken, String jobId) async {
-    await dio.put(
-      '/jobs/$jobId/reject',
-      options: Options(headers: {'Authorization': 'Bearer $accessToken'}),
-    );
+  static Future<void> scanArrival(String jobId, String token, String accessToken) async {
+    await dio.put('/jobs/$jobId/arrival/scan',
+      data: {'token': token},
+      options: Options(headers: {'Authorization': 'Bearer $accessToken'}));
   }
 }
