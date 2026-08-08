365be7d2b825dbf25950fa2890d8bdb011f8f784
Eshanhasitha <eshanhasitha5@gmail.com>
2026-08-08 22:41:04 +0530

    Fix JDK and Gradle problem

--- mobile/android/app/build.gradle.kts
+++ mobile/android/app/build.gradle.kts
@@ -7,8 +7,9 @@ plugins {
 
 android {
     namespace = "com.lankaserve.mobile"
-    compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    compileSdk = 36
+    buildToolsVersion = "34.0.0"
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
@@ -20,14 +21,11 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.lankaserve.mobile"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
-        targetSdk = flutter.targetSdkVersion
-        versionCode = flutter.versionCode
-        versionName = flutter.versionName
+        targetSdk = 36
+        versionCode = 1
+        versionName = "1.0.0"
     }
 
     buildTypes {
--- mobile/android/settings.gradle.kts
+++ mobile/android/settings.gradle.kts
@@ -1,4 +1,8 @@
 pluginManagement {
+    // Force clear conflicting system properties that cause AndroidLocationsBuildService failure
+    System.clearProperty("ANDROID_PREFS_ROOT")
+    System.clearProperty("ANDROID_SDK_HOME")
+
     val flutterSdkPath =
         run {
             val properties = java.util.Properties()
@@ -19,8 +23,9 @@ pluginManagement {
 
 plugins {
     id("dev.flutter.flutter-plugin-loader") version "1.0.0"
-    id("com.android.application") version "8.11.1" apply false
-    id("org.jetbrains.kotlin.android") version "2.2.20" apply false
+    id("com.android.application") version "8.9.1" apply false
+    id("com.android.library") version "8.9.1" apply false
+    id("org.jetbrains.kotlin.android") version "2.1.0" apply false
     id("com.google.gms.google-services") version "4.4.2" apply false
 }
 
